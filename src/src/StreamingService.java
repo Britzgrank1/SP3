1059db502e0e6a2f0e09328b77aeda01219152ae
tobiashardoe <tobias.hardoe@gmail.com>
2025-11-06 12:45:42 +0100

    end session + user bliver gemt

--- src/src/StreamingService.java
+++ src/src/StreamingService.java
@@ -17,4 +17,16 @@ public class StreamingService {
 
     }
 
+
+
+    public void endSession(){
+        ui.displayMsg("Exiting Chill");
+        ArrayList<String> userData = new ArrayList<>();
+        for (User user: users){
+            String s = user.toString();
+            userData.add(s);
+        }
+        io.saveData(userData, "data/userData.csv", "Username, Password");
+    }
+
 }
