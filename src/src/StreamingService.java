cef180882e87c00eafada66e4e5e2474dacb1998
tobiashardoe <tobias.hardoe@gmail.com>
2025-11-07 10:24:55 +0100

    rettet i startsession

--- src/src/StreamingService.java
+++ src/src/StreamingService.java
@@ -11,17 +11,18 @@ public class StreamingService {
     User currentUser;
 
     public void startSession(){
-        ui.displayMsg("Welcome to Chill");
-        System.out.println();
+
         int choice = ui.promptNumeric("Press 1 for login\nPress 2 for create new user.");
 
         if(choice == 1){
             ui.displayMsg("Going to login screen");
             userLogin();
-        } else if (choice == 2){
+        } else if (choice == 2) {
             ui.displayMsg("Going to create user screen");
-            createUser();} //Tror der skal være currentUser.username, currentUser.Password her - Seif
-            else if (choice == 0){
+            createUser(); //Tror der skal være currentUser.username, currentUser.Password her - Seif
+            ui.displayMsg("Returning to main menu");
+            startSession();
+        }    else if (choice == 0){
                 endSession();
             }
             else {
@@ -43,23 +44,20 @@ public class StreamingService {
 
     }
 
-
     private void createUser(){
         String username = TextUI.promptText("Insert username:"); //String foran username tror jeg burde fjernes -seif
         String password = TextUI.promptText("Insert password:"); //String foran Password tror jeg burde fjernes -seif
 
-        User user = new User(username, password);
+        User user = new User(username, password); // creater nye users
+        users.add(user); // adder users
 
-        ArrayList<String> userData = new ArrayList<>();
+        ArrayList<String> userData = new ArrayList<>(); // ArrayList til at gemme users
         for (User u: users){
             String s = u.toString();
             userData.add(s);
         }
-        io.saveData(userData, "Data/userData.csv", "Username, Password");
+        io.saveData(userData, "Data/userData.csv", "Username, Password"); // gemmer users til til userData.csv
 
-        System.out.println("New user added " + username);
-
-
+        System.out.println("New user added: " + username); // printer en besked at brugeren er gemt
     }
-
 }
